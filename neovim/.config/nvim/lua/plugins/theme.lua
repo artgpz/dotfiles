d9a22c696d77165fe75710460f12862bd94942fc
artgp <git@arturoguzmanperez.com>
2025-10-02 08:24:12 +0100

    fix: pin lazy version to avoid compiling issues with nvim treesitter

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -4,6 +4,15 @@ return {
     name = "catppuccin",
     lazy = true,
     priority = 1000,
+    specs = {
+      "akinsho/bufferline.nvim",
+      init = function()
+        local bufline = require("catppuccin.special.bufferline")
+        function bufline.get()
+          return bufline.get_theme()
+        end
+      end,
+    },
   },
   {
     "LazyVim/LazyVim",
