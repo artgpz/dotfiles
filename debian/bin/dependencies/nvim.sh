ba52a838a4f288b3e4388efdd8273ef271261b2d
artgp <git@arturoguzmanperez.com>
2026-03-03 15:43:07 +0000

    fix: move all folders

--- debian/bin/dependencies/nvim.sh
+++ debian/bin/dependencies/nvim.sh
@@ -16,5 +16,10 @@ if [[ "$arch" != 'none' ]]; then
   curl -OLsS https://github.com/neovim/neovim/releases/download/v$VERSION/nvim-$arch.tar.gz
   tar xzvf nvim-$arch.tar.gz
   mv ./nvim-$arch/bin/nvim /usr/local/bin
+  mv ./nvim-$arch/lib/nvim /usr/local/lib
+  mv ./nvim-$arch/share/applications /usr/local/share
+  mv ./nvim-$arch/share/icons /usr/local/share
+  mv ./nvim-$arch/share/man /usr/local/share
+  mv ./nvim-$arch/share/nvim /usr/local/share
   echo -e "${GREEN} nvim installed ${CLEAR}"
 fi
